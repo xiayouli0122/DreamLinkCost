c167ec1873cde42f5f58a567692600c7254d10d2
Yuri Xia <xlog2013@163.com>
2015-10-15 15:42:31 +0800

    增加一个启动页

---
+++ app/src/main/java/com/yuri/dreamlinkcost/SplashActivity.java
@@ -0,0 +1,58 @@
+package com.yuri.dreamlinkcost;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.THEME_YURI); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+        configSplash.setPathSplash(SplashConstants.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(2000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.accent); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(2000);
+        configSplash.setPathSplashFillColor(R.color.colorPrimary); //path object filling color
+
+
+        //Customize Title
+        configSplash.setTitleSplash("美好的生活从现在开始");
+        configSplash.setTitleTextColor(R.color.colorPrimary);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(2000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+//        configSplash.setTitleFont(SplashConstants.DITI_SWEET_FONT); //provide string to your font located in assets/fonts/
+    }
+
+    @Override
+    public void animationsFinished() {
+        Intent intent = new Intent();
+        intent.setClass(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+}
