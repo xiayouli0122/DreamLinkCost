d006255f6cfd0a362892cb3db02ffdccf6a55b07
Yuri Xia <xlog2013@163.com>
2016-01-16 17:09:15 +0800

    modify

---
+++ app/src/main/java/com/yuri/dreamlinkcost/model/OnDeleteItemListener.java
@@ -0,0 +1,9 @@
+package com.yuri.dreamlinkcost.model;
+
+/**
+ * Created by Yuri on 2016/1/16.
+ */
+public interface OnDeleteItemListener {
+    void onDeleteSucess();
+    void onDeleteFail(String msg);
+}
